e8b9dc43a340942c534ebc7b9b6a22d283faebe3
Sourav Saha <souravsahapartho@gmail.com>
2025-08-03 23:00:04 +0600

    Create add or remove elements.js

---
+++ array/add or remove elements.js
@@ -0,0 +1,21 @@
+let tourist=['Bandarban','Coxs bazar','Sitakundu'];
+
+tourist.push('Guliyakhali');
+
+tourist.push('Sylhet','Rangamati');
+
+console.log(tourist);
+
+tourist.pop();
+
+console.log(tourist);
+
+
+/*
+Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output
+*/
