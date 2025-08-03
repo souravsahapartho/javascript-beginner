556cf0da785f889e7811858240b9002d860ffbf1
Sourav Saha <souravsahapartho@gmail.com>
2025-08-03 22:55:50 +0600

    Create declare an array.js

---
+++ array/declare an array.js
@@ -0,0 +1,15 @@
+let fruits=['mango','banana','apple','orange','watermelon'];
+
+console.log(fruits[3]);
+
+fruits[2]='jambura';
+console.log(fruits);
+
+
+/*
+Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
