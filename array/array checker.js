93c8f6ab494af2f028d7823bd6e48c1c48195986
Sourav Saha <souravsahapartho@gmail.com>
2025-08-03 23:16:18 +0600

    Create array checker.js

---
+++ array/array checker.js
@@ -0,0 +1,19 @@
+let fruits=['apple','banana','mango'];
+
+if(Array.isArray(fruits)){
+    console.log("Array");
+}
+else{
+    console.log("Not array");
+}
+
+/*
+Checking if it's an Array
+Instructions:
+
+Create different variables, each containing either an array or a non-array value.
+
+Now use isArray to check if each variable is an array.
+
+Print a message to the console indicating whether each variable is an array or not.
+*/
