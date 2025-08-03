d33adfed0a1bb9cfef1aff70ab22d806f28f4b0d
Sourav Saha <souravsahapartho@gmail.com>
2025-08-03 09:52:22 +0600

    Create sum of odd-even.js

---
+++ while/sum of odd-even.js
@@ -0,0 +1,42 @@
+console.log("Sum of odd numbers from 91 to 129: ")
+let sum=0;
+let i=91;
+while(i<=129){
+    if(i%2!==0){
+        sum+=i;
+    }
+    i++;
+}
+console.log(sum);
+
+
+console.log("\nSum of odd numbers from 51 to 85: ")
+let sumeven=0;
+let j=51;
+while(j<=85){
+    if(j%2!==0){
+        sumeven+=j;
+    }
+    j++;
+}
+console.log(sumeven);
+
+
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+/*programming hero*/
