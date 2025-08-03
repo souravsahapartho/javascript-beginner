bd104953329a8d0872b0a19016c706d39aebec48
Sourav Saha <souravsahapartho@gmail.com>
2025-08-03 09:07:18 +0600

    Create odd even.js

---
+++ for/odd even.js
@@ -0,0 +1,35 @@
+
+console.log("Odd numbers from 61 to 100: ");
+for(let i=61; i<=100; i++){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
+
+console.log("\nEven numbers from 78 to 98: ");
+for(let i=78; i<=98; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+
+
+
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
