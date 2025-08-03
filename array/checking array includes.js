37fd66f4b911487b8f221805950200e281b24dc3
Sourav Saha <souravsahapartho@gmail.com>
2025-08-03 23:10:22 +0600

    Create checking array includes.js

---
+++ array/checking array includes.js
@@ -0,0 +1,19 @@
+let books=['Physics','Data-Structure','Chemistry','Bangla','English'];
+
+if(books.includes('Chemistry')){
+    console.log("Found");
+}
+else{
+    console.log("Not found");
+}
+
+/*
+Checking Array Membership with ‘includes’
+Instructions:
+
+Create an array of books containing different book.
+
+Use the includes method to check if the array contains a javascript book.
+
+Print a message to the console indicating whether the element is present in the array or not.
+*/
