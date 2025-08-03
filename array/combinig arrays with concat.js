98a90d7db29505eb72d587524a59a7b551c3ea1e
Sourav Saha <souravsahapartho@gmail.com>
2025-08-03 23:24:54 +0600

    Create combinig arrays with concat.js

---
+++ array/combinig arrays with concat.js
@@ -0,0 +1,21 @@
+let fruits=['apple','banana'];
+let flowers=['rose','jesmine'];
+
+console.log(fruits);
+console.log(flowers);
+
+let concatstring= fruits.concat(flowers);
+
+console.log(concatstring);
+
+
+
+
+/*
+Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+*/
