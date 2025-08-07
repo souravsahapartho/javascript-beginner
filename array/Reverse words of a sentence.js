c65130ee93e5ed8a0363b8e7b05b800b581fc1c5
Sourav Saha <souravsahapartho@gmail.com>
2025-08-07 20:44:07 +0600

    Create Reverse words of a sentence.js

---
+++ array/Reverse words of a sentence.js
@@ -0,0 +1,20 @@
+const statement = 'I am a hard working person'
+let result=[];
+for(let i=0; i<statement.length; i++){
+result= statement[i]+result;
+}
+console.log(result);
+
+
+
+
+
+/*
+Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+Input: const statement = 'I am a hard working person'
+
+Output:
+
+'person working hard a am I'
+*/
