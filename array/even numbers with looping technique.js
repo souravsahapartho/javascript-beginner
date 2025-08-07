4cd11014c07495b1d3e6c7e010965e045decd75a
Sourav Saha <souravsahapartho@gmail.com>
2025-08-07 20:32:10 +0600

    Create even numbers with looping technique.js

---
+++ array/even numbers with looping technique.js
@@ -0,0 +1,23 @@
+const numbers=[12,98,5,41,23,78,46];
+const evenNumbers=[];
+for(let i=0; i<numbers.length; i++){
+    if(numbers[i]%2===0){
+   evenNumbers.push(numbers[i]);
+    }
+}
+
+console.log(evenNumbers);
+
+
+
+/*
+Write a JavaScript code to get the even numbers from an array using any looping technique.
+
+Input: const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+Output:
+
+[12, 98, 76, 46]
+
+
+*/
