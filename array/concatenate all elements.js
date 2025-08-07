4718bd542b88a7539638373219cfed186017fd75
Sourav Saha <souravsahapartho@gmail.com>
2025-08-07 20:38:04 +0600

    Create concatenate all elements.js

---
+++ array/concatenate all elements.js
@@ -0,0 +1,20 @@
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+let result='';
+for(let i=0; i<numbers.length; i++){
+    result+=numbers[i];
+}
+
+console.log(result);
+
+
+
+
+/*
+Use a for...of loop to concatenate all the elements of an array into a single string.
+
+Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+Output:
+
+'TomTimTinTik'
+*/
