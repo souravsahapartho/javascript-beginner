ec5603ba18de824a1e807c1da7fe49e9f88d0855
Sourav Saha <souravsahapartho@gmail.com>
2025-08-07 20:25:15 +0600

    Create reverse wuthout reverse method.js

---
+++ array/reverse wuthout reverse method.js
@@ -0,0 +1,15 @@
+const colors=['red','blue','green','yellow','orange'];
+for(let i=colors.length-1; i>=0; i--){
+    console.log(colors[i]);
+}
+
+
+/*
+Write a JavaScript code to reverse the array colors without using the reverse method.
+
+Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+Output:
+
+['orange', 'yellow', 'green', 'blue', 'red']
+*/
