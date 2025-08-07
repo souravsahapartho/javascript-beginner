221d9b07bf17cf96e0a3792175484bc0bf96647e
Sourav Saha <souravsahapartho@gmail.com>
2025-08-07 20:58:42 +0600

    Create copy array.js

---
+++ array/copy array.js
@@ -0,0 +1,19 @@
+const A=[1,2,3];
+let B=A;
+B[0]=99;
+console.log(B);
+
+
+
+
+/*
+Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+
+Input: [1, 2, 3]
+
+Expected Output:
+
+Original: [1, 2, 3] Copy: [99, 2, 3]
+
+
+*/
