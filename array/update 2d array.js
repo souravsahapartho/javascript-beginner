e4ef999b831783ba5b6aa87072d2ff219e0281be
Sourav Saha <souravsahapartho@gmail.com>
2025-08-08 10:37:11 +0600

    Create update 2d array.js

---
+++ array/update 2d array.js
@@ -0,0 +1,30 @@
+const numbers = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+
+numbers[1][0] = 99;
+console.log(numbers);
+
+
+
+
+/*
+Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+input:
+
+[
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]
+Expected Array:
+
+[
+  [1, 2],
+  [99, 4],
+  [5, 6]
+]
+*/
