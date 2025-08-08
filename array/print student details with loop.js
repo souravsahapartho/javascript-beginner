9e0193e4c6d067c5f0dd4caa43ae4ff123d6532a
Sourav Saha <souravsahapartho@gmail.com>
2025-08-08 10:33:13 +0600

    Create print student details with loop.js

---
+++ array/print student details with loop.js
@@ -0,0 +1,23 @@
+const students = [
+    { name: "John", marks: 85 },
+    { name: "Alice", marks: 90 }
+];
+
+for (const student of students) {
+    console.log(student.name, ' scored ', student.marks);
+}
+
+
+
+
+/*
+Given an array of student objects, print each student’s name and marks.
+
+[
+  { name: "John", marks: 85 },
+  { name: "Alice", marks: 90 }
+]
+Expected ooutput:
+John scored 85
+Alice scored 90
+*/
