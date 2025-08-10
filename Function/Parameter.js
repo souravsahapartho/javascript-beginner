5988c472572142c7c041abc4ae57ab61a5ef7d72
Sourav Saha <souravsahapartho@gmail.com>
2025-08-10 19:55:58 +0600

    Create Parameter.js

---
+++ Function/Parameter.js
@@ -0,0 +1,13 @@
+/*
+Take four parameters. Multiply the four numbers and then return the result
+*/
+
+function mul(a,b,c,d){
+    mul=a*b*c*d;
+    return mul;
+}
+
+mul(1,2,3,4);
+console.log(mul);
+
+
