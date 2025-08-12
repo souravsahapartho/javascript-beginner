05757ba9c03ff17facd3ef5fb4711728c74b7998
Sourav Saha <souravsahapartho@gmail.com>
2025-08-12 23:33:21 +0600

    Update Temperature Convertion.js

--- Practice/Temperature Convertion.js
+++ Practice/Temperature Convertion.js
@@ -1,3 +1,12 @@
 /*
 Write a function to convert temperature from Celsius to Fahrenheit.
 */
+
+
+function CeltoFahrenheit(number){
+    const fahr=(number*(9/5))+32;
+    return fahr;
+}
+
+let number=CeltoFahrenheit(128);
+console.log(number);
