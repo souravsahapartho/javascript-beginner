1c10487149cddaebe3d010fa5c1e56360fce28ad
Sourav Saha <souravsahapartho@gmail.com>
2025-08-12 20:01:36 +0600

    Create Math.js

---
+++ beginner-practice/Math.js
@@ -0,0 +1,30 @@
+// const Math={
+//     min: function min(num1, num2){
+//         return num2;
+//     }
+// }
+
+// console.log(Math.min);
+
+const min = Math.min(45, 21, 56, 99, 126, -99);
+console.log(min);
+
+const max = Math.max(45, 21, 56, 99, 126, -99);
+console.log(max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(5 - 88));//one kind od mod |mod|
+
+console.log(Math.round(2.33334));
+
+console.log(Math.floor(4.86));
+
+console.log(Math.ceil(4.86));
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
+
+const rand=Math.random()*10;
+console.log(rand);
